b452b5c6c1703c10fe1ef748b8636a568a74912f
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2018-02-27 15:40:30 +0100

    Formatters and content negotiation

--- CityInfo.Api/Startup.cs
+++ CityInfo.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -16,7 +17,16 @@ namespace CityInfo.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(options =>
+                {
+                    //options.OutputFormatters.Clear();
+                    options.OutputFormatters.Add(
+                        new XmlDataContractSerializerOutputFormatter());
+                });
+
+            ////    Pascal case notation
+
             //.AddJsonOptions(o =>
             //{
             //    if (o.SerializerSettings.ContractResolver != null)
